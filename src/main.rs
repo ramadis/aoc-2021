fdfe472538160028c103b3c740d429401f75c7b1
ramadis <rramiro.o@hotmail.com>
2021-12-01 21:22:39 +0100

    [1] Read lines from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod files;
+
 fn main() {
-    println!("Hello, world!");
+    let vec = files::get_lines(String::from("/Users/rama/Documents/adventofcode/2021/rust/src/test.txt"));
+    for x in &vec {
+        println!("{}", x);
+    }
 }
